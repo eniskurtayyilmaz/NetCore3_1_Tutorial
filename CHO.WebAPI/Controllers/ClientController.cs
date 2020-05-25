1e3b6ccde14dd5c27b0226dd49d900ff4148aa3a
eniskurtayyilmaz <kurtayyilmaz@gmail.com>
2020-05-25 18:32:01 +0300

    Add project files.

---
+++ CHO.WebAPI/Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CHO.WebAPI.Controllers
+{
+  [Route("api/client")]
+  [ApiController]
+  public class ClientController : ControllerBase
+  {
+    private static string[] dataArray = new string[4]
+    {
+      "Müşteri 1", "Müşteri 2", "Müşteri 3", "Müşteri 4"
+    };
+
+
+    [HttpGet]
+    public IActionResult GetClientList()
+    {
+      return Ok(dataArray);
+    }
+  }
+}
